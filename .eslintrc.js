7d2918d84f1d3fde639f3e06a126a9a27904f985
Hakan Han <eenestatli@gmail.com>
2021-02-21 22:28:20 +0300

    Ignore: Configuration files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,16 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
-};
+  extends: ['@react-native-community'],
+  rules: {
+    semi: ['error', 'never'],
+    'object-curly-spacing': ['error', 'always'],
+    'array-bracket-spacing': ['error', 'never'],
+    'react-native/no-inline-styles': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      'babel-module': {},
+    },
+  },
+}
