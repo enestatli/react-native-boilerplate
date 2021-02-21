f34f6ebc1be1c0c24bfe6bfac4f85b8a98fe50b3
Hakan Han <eenestatli@gmail.com>
2021-02-21 22:31:05 +0300

    New: Theme tested

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,11 +1,124 @@
-import { DefaultTheme } from '@react-navigation/native'
+import { DefaultTheme, DarkTheme } from '@react-navigation/native'
 
-const MyTheme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: 'red',
+const spacing = {
+  s: 8,
+  m: 16,
+  l: 24,
+  xl: 40,
+}
+
+const textVariants = {
+  header: {
+    fontFamily: 'Raleway',
+    fontSize: 36,
+    fontWeight: 'bold',
+  },
+  body: {
+    fontFamily: 'Merriweather',
+    fontSize: 16,
   },
 }
 
-export default MyTheme
+const breakPoints = {
+  smallPhone: 0,
+  phone: 321,
+  tablet: 768,
+}
+
+const palette = {
+  purple: '#5A31F4',
+  green: '#0ECD9D',
+  red: '#CD0E61',
+  black: '#0B0B0B',
+  white: 'white',
+}
+
+export const MyTheme = {
+  ...DefaultTheme,
+  dark: false,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: palette.purple,
+    background: palette.white,
+    card: palette.white,
+    text: palette.black,
+    border: palette.black,
+    notification: palette.red,
+  },
+  spacing: {
+    ...spacing,
+  },
+  textVariants: {
+    ...textVariants,
+  },
+  breakPoints: {
+    ...breakPoints,
+  },
+}
+
+export const MyDarkTheme = {
+  ...DarkTheme,
+  dark: true,
+  colors: {
+    ...DarkTheme.colors,
+    primary: palette.green,
+    background: palette.black,
+    card: palette.black,
+    text: palette.white,
+    border: palette.white,
+    notification: palette.red,
+  },
+  spacing: {
+    ...spacing,
+  },
+  textVariants: {
+    ...textVariants,
+  },
+  breakPoints: {
+    ...breakPoints,
+  },
+}
+
+// export const MyTheme = {
+//   ...DefaultTheme,
+//   colors: {
+//     ...DefaultTheme.colors,
+//     background: palette.white,
+//     foreground: palette.black,
+//     primary: palette.purple,
+//     success: palette.green,
+//     danger: palette.red,
+//     failure: palette.red,
+//   },
+//   spacing: {
+//     s: 8,
+//     m: 16,
+//     l: 24,
+//     xl: 40,
+//   },
+//   textVariants: {
+//     header: {
+//       fontFamily: 'Raleway',
+//       fontSize: 36,
+//       fontWeight: 'bold',
+//     },
+//     body: {
+//       fontFamily: 'Merriweather',
+//       fontSize: 16,
+//     },
+//   },
+//   breakpoints: {
+//     smallPhone: 0,
+//     phone: 321,
+//     tablet: 768,
+//   },
+// }
+
+// export const MyDarkTheme = {
+//   ...DarkTheme,
+//   colors: {
+//     ...DarkTheme.colors,
+//     background: palette.black,
+//     foreground: palette.white,
+//   },
+// }
