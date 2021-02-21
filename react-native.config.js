7320bbd865883513f5bec67ce8407e2df27219cd
Hakan Han <eenestatli@gmail.com>
2021-02-21 12:05:55 +0300

    New: CodePush&AppCenter configuration for Android

---
+++ react-native.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dependencies: {
+    'react-native-code-push': {
+      platforms: {
+        android: {
+          sourceDir: '/node_modules/react-native-code-push/android/app',
+        },
+      },
+    },
+  },
+};
