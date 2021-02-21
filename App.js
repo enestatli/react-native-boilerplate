25d49110180a7cba8c004a89cd6ff97896b24a22
Hakan Han <eenestatli@gmail.com>
2021-02-21 22:31:20 +0300

    Ignore: ...

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { SafeAreaView, Text, StatusBar } from 'react-native';
-import codePush from 'react-native-code-push';
+import React from 'react'
+import { SafeAreaView, Text, StatusBar } from 'react-native'
+import codePush from 'react-native-code-push'
 
 const App = () => {
   return (
@@ -10,14 +10,14 @@ const App = () => {
         <Text>Hello world!</Text>
       </SafeAreaView>
     </>
-  );
-};
+  )
+}
 
 //TODO: appcenter, react-native-bootsplash, react-native-codepush, react-native-localization, react-native-svg
 
 let codePushOptions = {
   checkFrequency: codePush.CheckFrequency.ON_APP_START,
   installMode: codePush.InstallMode.IMMEDIATE,
-};
+}
 
-export default codePush(codePushOptions)(App);
+export default codePush(codePushOptions)(App)
